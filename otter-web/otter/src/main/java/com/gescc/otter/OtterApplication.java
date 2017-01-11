1c3dea9db592dba685cad3272efc7a03807f321a
geine <bonos2@wisdo.me>
2017-01-11 19:26:08 +0900

    Project Init

---
+++ otter-web/otter/src/main/java/com/gescc/otter/OtterApplication.java
@@ -0,0 +1,12 @@
+package com.gescc.otter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtterApplication.class, args);
+	}
+}
